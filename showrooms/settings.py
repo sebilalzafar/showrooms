c9e4d80f113921da0658a15f2f9d7901154203ca
Bilal Zafar <bilalzafar7474@gmail.com>
2023-05-06 21:06:54 +0500

    requirement file

--- showrooms/settings.py
+++ showrooms/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j1%&=4*1ukb4uavjt=ejokg&d13gp_gqsig1bxje$+fbe&_9ii
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
