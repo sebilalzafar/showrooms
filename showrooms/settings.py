bf18a85fc28c0d5cf3739b049f79b36757ae7819
Bilal Zafar <bilalzafar7474@gmail.com>
2023-05-24 16:19:46 +0500

    push

--- showrooms/settings.py
+++ showrooms/settings.py
@@ -124,9 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+
 MEDIA_ROOT = BASE_DIR / 'static'
 STATIC_ROOT = 'static'
 
