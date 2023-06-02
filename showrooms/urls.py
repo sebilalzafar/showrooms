24fd98e5feb41dcbcf75824545b9e166be8a6d61
Bilal Zafar <bilalzafar7474@gmail.com>
2023-06-02 20:48:41 +0500

    htmx setting

--- showrooms/urls.py
+++ showrooms/urls.py
@@ -2,13 +2,16 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+
+
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
     path('', include('core.urls')),
     path('', include('order_management.urls')),
-
+    
 ]
-if settings.DEBUG:
+if  settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
     
-    
+
